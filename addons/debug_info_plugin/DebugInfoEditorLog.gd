90af41eb6d302a8e10b24b69185eb61c6dabd817
coderbloke <75695649+coderbloke@users.noreply.github.com>
2023-05-31 19:24:52 +0200

    Editor log is fully replicated (status before test)

--- addons/debug_info_plugin/DebugInfoEditorLog.gd
+++ addons/debug_info_plugin/DebugInfoEditorLog.gd
@@ -68,19 +68,21 @@ class LogFilter:
 var messages: Array[LogMessage] = []
 var type_filter_map = { }
 
-@onready var clear_button := %ClearButton
-@onready var copy_button := %CopyButton
+@onready var log := %Log as RichTextLabel
+
+@onready var clear_button := %ClearButton as Button
+@onready var copy_button := %CopyButton as Button
 	
-@onready var collapse_button := %CollapseButton
+@onready var collapse_button := %CollapseButton as Button
 var collapse := false
 		
 var tool_button: Button # TODO: intro state for tool_button, make _update_function, call it from setter also
 	
-@onready var show_search_button := %ShowSearchButton
-@onready var search_box := %SearchBox
+@onready var show_search_button := %ShowSearchButton as Button
+@onready var search_box := %SearchBox as LineEdit
 
-@onready var save_state_timer := %SaveStateTimer
-@onready var log := %Log
+@onready var is_loading_state := false
+@onready var save_state_timer := %SaveStateTimer as Timer
 
 
 func _ready():
@@ -99,6 +101,7 @@ func _ready():
 	type_filter_map[MessageType.EDITOR] = LogFilter.new(MessageType.EDITOR, %EditorFilterButton)
 	for filter in type_filter_map.values():
 		filter.toggled.connect(_set_filter_active)
+		
 
 
 func _update_theme():
@@ -166,6 +169,7 @@ func _notification(what: int):
 			_load_state()
 		NOTIFICATION_READY:
 			_update_theme()
+			_load_state()
 		NOTIFICATION_THEME_CHANGED:
 			_update_theme()
 			_rebuild_log()
@@ -175,58 +179,195 @@ func _set_collapse(collapse: bool):
 	self.collapse = collapse
 	_start_state_save_timer()
 	_rebuild_log()
+	
 
 func _start_state_save_timer():
-	pass
+	if not is_loading_state:
+		save_state_timer.start()
 
 
 func _save_state():
-	pass
+	var editor_path := EditorPaths.new()
+	var config := ConfigFile.new()
+	# Load and amend existing config if it exists.
+	config.load(editor_path.get_project_settings_dir().path_join("editor_layout.cfg"))
+
+	const section := "editor_log"
+	for key in type_filter_map:
+		config.set_value(section, "log_filter_" + str(key), type_filter_map[key].is_active())
+
+	config.set_value(section, "collapse", collapse)
+	config.set_value(section, "show_search", search_box.is_visible())
+
+	config.save(editor_path.get_project_settings_dir().path_join("editor_layout.cfg"))
 
 
 func _load_state():
-	pass
+	if not is_node_ready():
+		return
 	
+	is_loading_state = true
+
+	var editor_path := EditorPaths.new()
+	var config := ConfigFile.new()
+	config.load(editor_path.get_project_settings_dir().path_join("editor_layout.cfg"))
+
+	# Run the below code even if config.load returns an error, since we want the defaults to be set even if the file does not exist yet.
+	const section := "debug_info_editor_log"
+	for key in type_filter_map:
+		type_filter_map[key].active = config.get_value(section, "log_filter_" + str(key), true) as bool
+
+	collapse = config.get_value(section, "collapse", false)
+	collapse_button.button_pressed = collapse
+	var show_search := config.get_value(section, "show_search", true) as bool
+	search_box.visible = show_search
+	show_search_button.button_pressed = show_search
+
+	is_loading_state = false
+
 
 func _clear_request():
-	pass
-	
+	log.clear()
+	messages.clear()
+	_reset_message_counts()
+	if tool_button != null:
+		tool_button.icon = Texture2D.new()
+
 
 func _copy_request():
-	pass
-	
+	var text := log.get_selected_text()
+
+	if text.is_empty():
+		text = log.get_parsed_text()
+
+	if not text.is_empty():
+		DisplayServer.clipboard_set(text)
+
 
 func clear():
 	_clear_request()
 
 
 func _process_message(msg: String, type: MessageType):
-	pass
+	if messages.size() > 0 and messages[messages.size() - 1].text == msg and messages[messages.size() - 1].type == type:
+		# If previous message is the same as the new one, increase previous count rather than adding another
+		# instance to the messages list.
+		var previous := messages[messages.size() - 1]
+		previous.count += 1
+		_add_log_line(previous, collapse)
+	else:
+		# Different message to the previous one received.
+		var message := LogMessage.new(msg, type)
+		_add_log_line(message, type)
+		messages.push_back(message)
+
+	type_filter_map[type].message_count += 1
 	
 
 func add_message(msg: String, type: MessageType):
-	pass
-	
+	var lines := msg.split("\n", true)
+	for line in lines:
+		_process_message(line, type)
+
 
 func _rebuild_log():
-	pass
+	if log == null:
+		return
 	
+	log.clear()
 
-func _add_log_line(message: LogMessage, replace_previous: bool):
-	pass
+	for msg in messages:
+		if collapse:
+			# If collapsing, only log one instance of the message.
+			_add_log_line(msg)
+		else:
+			# If not collapsing, log each instance on a line.
+			for i in msg.count:
+				_add_log_line(msg)
+
+
+func _add_log_line(message: LogMessage, replace_previous: bool = false):
+	if not is_inside_tree():
+		# The log will be built all at once when it enters the tree and has its theme items.
+		return
+
+#	if log.is_updating(): # Non-existing in GDScript
+#		# The new message arrived during log RTL text processing/redraw (invalid BiDi control characters / font error), ignore it to avoid RTL data corruption.
+#		return
+
+	# Only add the message to the log if it passes the filters.
+	var filter_active := (type_filter_map[message.type] as LogFilter).active
+	var search_text := search_box.text
+	var search_match := search_text.is_empty() or message.text.findn(search_text) > -1
+
+	if !filter_active or !search_match:
+		return
+
+	if replace_previous:
+		# Remove last line if replacing, as it will be replace by the next added line.
+		# Why "- 2"? RichTextLabel is weird. When you add a line with add_newline(), it also adds an element to the list of lines which is null/blank,
+		# but it still counts as a line. So if you remove the last line (count - 1) you are actually removing nothing...
+		log.remove_paragraph(log.get_paragraph_count() - 2)
+
+	match message.type:
+		MessageType.STD:
+			pass
+		MessageType.STD_RICH:
+			pass
+		MessageType.ERROR:
+			log.push_color(theme_cache.error_color)
+			var icon := theme_cache.error_icon
+			log.add_image(icon)
+			log.add_text(" ")
+			if tool_button != null:
+				tool_button.icon = icon
+		MessageType.WARNING:
+			log.push_color(theme_cache.warning_color)
+			var icon := theme_cache.warning_icon
+			log.add_image(icon)
+			log.add_text(" ")
+			if tool_button != null:
+				tool_button.set_icon(icon)
+		MessageType.EDITOR:
+			# Distinguish editor messages from messages printed by the project
+			log.push_color(theme_cache.message_color)
+
+	# If collapsing, add the count of this message in bold at the start of the line.
+	if collapse and message.count > 1:
+		log.push_bold()
+		log.add_text("(%s) " % message.count)
+		log.pop()
+
+	if message.type == MessageType.STD_RICH:
+		log.append_text(message.text)
+	else:
+		log.add_text(message.text)
+
+	# Need to use pop() to exit out of the RichTextLabels current "push" stack.
+	# We only "push" in the above switch when message type != STD and RICH, so only pop when that is the case.
+	if message.type != MessageType.STD && message.type != MessageType.STD_RICH:
+		log.pop()
+
+	log.newline()
 	
 
 func _set_filter_active(active: bool, type: MessageType):
-	pass
+	type_filter_map[type].set_active(active)
+	_start_state_save_timer()
+	_rebuild_log()
 
 
 func _set_search_visible(visible):
-	pass
+	search_box.visible = visible
+	if visible:
+		search_box.grab_focus()
+	_start_state_save_timer()
 
 
 func _search_changed(new_text):
-	pass
+	_rebuild_log()
 
 
 func _reset_message_counts():
-	pass
+	for key in type_filter_map:
+		type_filter_map[key].message_count = 0
