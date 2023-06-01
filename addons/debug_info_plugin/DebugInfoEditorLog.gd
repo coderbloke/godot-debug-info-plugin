0abbf01297823ac607a2a1c1b4cd970a1fc8c340
coderbloke <75695649+coderbloke@users.noreply.github.com>
2023-06-01 16:48:32 +0200

    Timestamp added
    Visibility can be turned on/off. Setting are saved/loaded. Can be selected if tracking is done for each element, or only first/last (in case of repetitive messages)

--- addons/debug_info_plugin/DebugInfoEditorLog.gd
+++ addons/debug_info_plugin/DebugInfoEditorLog.gd
@@ -13,7 +13,10 @@ class LogMessage:
 
 	var text: String
 	var type: MessageType
-	var count = 1
+	var count := 1
+	var track_all_timestamps := false
+	var timestamps: PackedInt64Array = []
+	var timestamp_positions: PackedInt64Array = []
 	
 	func _init(text: String = "", type: MessageType = MessageType.STD):
 		self.text = text
@@ -71,6 +74,8 @@ class LogFilter:
 var messages: Array[LogMessage] = []
 var type_filter_map = { }
 
+var track_all_timestamps := false # If true, all timestamp will be stored for repetitive message, which increase memory usage, in certain conditin (quickly repeated same message several times)
+
 @onready var log := %Log as RichTextLabel
 
 @onready var clear_button := %ClearButton as Button
@@ -87,6 +92,9 @@ var tool_button: Button # TODO: intro state for tool_button, make _update_functi
 @onready var is_loading_state := false
 @onready var save_state_timer := %SaveStateTimer as Timer
 
+@onready var timestamp_filter_button := %TimestampFilterButton as Button
+var timestamp_visible := true
+
 
 func _ready():
 	save_state_timer.timeout.connect(_save_state)
@@ -97,6 +105,7 @@ func _ready():
 	copy_button.pressed.connect(_copy_request)
 	collapse_button.toggled.connect(_set_collapse)
 	show_search_button.toggled.connect(_set_search_visible)
+	timestamp_filter_button.toggled.connect(_set_timestamp_visible)
 	
 	type_filter_map[MessageType.STD] = LogFilter.new(MessageType.STD, %StdFilterButton)
 	type_filter_map[MessageType.ERROR] = LogFilter.new(MessageType.ERROR, %ErrorFilterButton)
@@ -157,7 +166,7 @@ func _update_theme():
 		collapse_button.icon = get_theme_icon("CombineLines", "EditorIcons")
 		show_search_button.icon = get_theme_icon("Search", "EditorIcons")
 		search_box.right_icon = get_theme_icon("Search", "EditorIcons")
-
+		
 	theme_cache.error_color = get_theme_color("error_color", "Editor")
 	theme_cache.error_icon = get_theme_icon("Error", "EditorIcons")
 	theme_cache.warning_color = get_theme_color("warning_color", "Editor")
@@ -184,6 +193,12 @@ func _set_collapse(collapse: bool):
 	_rebuild_log()
 	
 
+func _set_timestamp_visible(visible: bool):
+	self.timestamp_visible = visible
+	_start_state_save_timer()
+	_rebuild_log()
+	
+
 func _start_state_save_timer():
 	if not is_loading_state:
 		save_state_timer.start()
@@ -201,6 +216,8 @@ func _save_state():
 
 	config.set_value(section, "collapse", collapse)
 	config.set_value(section, "show_search", search_box.visible)
+	config.set_value(section, "show_timestamp", timestamp_visible)
+	config.set_value(section, "track_all_timestamps", track_all_timestamps)
 
 	config.save(editor_path.get_project_settings_dir().path_join("editor_layout.cfg"))
 
@@ -225,6 +242,8 @@ func _load_state():
 	var show_search := config.get_value(section, "show_search", true) as bool
 	search_box.visible = show_search
 	show_search_button.button_pressed = show_search
+	timestamp_visible = config.get_value(section, "show_timestamp", true)
+	track_all_timestamps = config.get_value(section, "track_all_timestamps", false)
 
 	is_loading_state = false
 
@@ -251,26 +270,37 @@ func clear():
 	_clear_request()
 
 
-func _process_message(msg: String, type: MessageType):
+func _process_message(msg: String, timestamp: int, type: MessageType):
 	if messages.size() > 0 and messages[messages.size() - 1].text == msg and messages[messages.size() - 1].type == type:
 		# If previous message is the same as the new one, increase previous count rather than adding another
 		# instance to the messages list.
 		var previous := messages[messages.size() - 1]
+		if track_all_timestamps or previous.track_all_timestamps or previous.count <= 1: # even if turned-off, but just now, we keep last element 
+			previous.timestamps.append(timestamp)
+			previous.timestamp_positions.append(previous.count)
+		else:
+			previous.timestamps[previous.timestamps.size() - 1] = timestamp
+			previous.timestamp_positions[previous.timestamp_positions.size() - 1] = previous.count
 		previous.count += 1
-		_add_log_line(previous, collapse)
+		previous.track_all_timestamps = track_all_timestamps
+		_add_log_line(previous, collapse, previous.timestamps.size() - 1)
 	else:
 		# Different message to the previous one received.
 		var message := LogMessage.new(msg, type)
-		_add_log_line(message)
+		message.timestamps.append(timestamp)
+		message.timestamp_positions.append(0)
+		message.track_all_timestamps = track_all_timestamps
+		_add_log_line(message, false, 0)
 		messages.push_back(message)
 
 	type_filter_map[type].message_count += 1
 	
 
 func add_message(msg: String, type: MessageType):
+	var timestamp := Time.get_unix_time_from_system()
 	var lines := msg.split("\n", true)
 	for line in lines:
-		_process_message(line, type)
+		_process_message(line, timestamp, type)
 
 
 func _rebuild_log():
@@ -282,14 +312,18 @@ func _rebuild_log():
 	for msg in messages:
 		if collapse:
 			# If collapsing, only log one instance of the message.
-			_add_log_line(msg)
+			_add_log_line(msg, false, msg.timestamps.size() - 1)
 		else:
 			# If not collapsing, log each instance on a line.
+			var timestamp_index := 0
 			for i in msg.count:
-				_add_log_line(msg)
+				if msg.timestamp_positions.size() > timestamp_index + 1 \
+						and msg.timestamp_positions[timestamp_index + 1] == i:
+							timestamp_index += 1
+				_add_log_line(msg, false, timestamp_index if msg.timestamp_positions[timestamp_index] == i else -1)
 
 
-func _add_log_line(message: LogMessage, replace_previous: bool = false):
+func _add_log_line(message: LogMessage, replace_previous: bool = false, timestamp_index: int = -1):
 	if not is_inside_tree():
 		# The log will be built all at once when it enters the tree and has its theme items.
 		return
@@ -319,13 +353,31 @@ func _add_log_line(message: LogMessage, replace_previous: bool = false):
 			pass
 		MessageType.ERROR:
 			log.push_color(theme_cache.error_color)
+		MessageType.WARNING:
+			log.push_color(theme_cache.warning_color)
+		MessageType.EDITOR:
+			pass
+
+	if timestamp_visible:
+		if timestamp_index >= 0 and timestamp_index < message.timestamps.size():
+			log.add_text("[" + Time.get_datetime_string_from_unix_time(message.timestamps[timestamp_index]) + "] ")
+		else:
+			log.add_text("[       . . .       ] ")
+
+	match message.type:
+		MessageType.STD:
+			pass
+		MessageType.STD_RICH:
+			pass
+		MessageType.ERROR:
+#			log.push_color(theme_cache.error_color)
 			var icon := theme_cache.error_icon
 			log.add_image(icon)
 			log.add_text(" ")
 			if tool_button != null:
 				tool_button.icon = icon
 		MessageType.WARNING:
-			log.push_color(theme_cache.warning_color)
+#			log.push_color(theme_cache.warning_color)
 			var icon := theme_cache.warning_icon
 			log.add_image(icon)
 			log.add_text(" ")
@@ -374,3 +426,15 @@ func _search_changed(new_text):
 func _reset_message_counts():
 	for key in type_filter_map:
 		type_filter_map[key].message_count = 0
+
+func print(msg):
+	add_message(str(msg), MessageType.STD)
+
+func print_rich(msg):
+	add_message(str(msg), MessageType.STD_RICH)
+
+func printerr(msg):
+	add_message(str(msg), MessageType.ERROR)
+
+func print_warning(msg):
+	add_message(str(msg), MessageType.ERROR)
